b7849f2459848aaef802e365cae5b37819203d28
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2023-01-02 14:33:11 -0300

    update script.html

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-function botaoclick(){
-    var alert = confirm("VOCE TEM CERTEZA?")
-    if (alert==true){
-    var botao = document.getElementsByClassName("button")
-    var reset = document.getElementById("reset")
-    var opcao1 = document.getElementById("op1")
-    var opcao2 = document.getElementById("op2")
-    var opcao3 = document.getElementById("op3")
-    var opcao4 = document.getElementById("op4")
+var botao = document.getElementsByClassName("button")
+var reset = document.getElementById("reset")
+var next = document.getElementById("next")
+var opcao1 = document.getElementById("op1")
+var opcao2 = document.getElementById("op2")
+var opcao3 = document.getElementById("op3")
+var opcao4 = document.getElementById("op4")
 
+
+
+function botaoclick(){
+    if (alert==true){
+    var alert = confirm("VOCE TEM CERTEZA?")
     opcao1.style.backgroundColor= "red"
-    opcao2.style.backgroundColor= "green"
     opcao3.style.backgroundColor= "red"
     opcao4.style.backgroundColor= "red"
+    opcao2.style.backgroundColor= "rgba(0, 255, 51, 0.697)"
+
+
+    
 
     reset.style.display = 'block'
+    
 
     }
-    else{
     
-    }
+
+
     
 
 }
+
 
 function resetpage(){
     window.location.reload();
